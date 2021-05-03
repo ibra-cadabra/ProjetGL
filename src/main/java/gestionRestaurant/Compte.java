343b87b4ab701409f3aa4fdc438ecb6dc8cc82ea
Coulibaly <briwinzin10@gmail.com>
2021-05-03 17:53:16 +0200

    Commit de la classe Compte

---
+++ src/main/java/gestionRestaurant/Compte.java
@@ -0,0 +1,17 @@
+package gestionRestaurant;
+
+public class Compte {
+	String login;
+	String password;
+	String role;
+	
+	public Compte(String login, String password, String role) {
+		super();
+		this.login = login;
+		this.password = password;
+		this.role = role;
+	}
+	
+	
+
+}
