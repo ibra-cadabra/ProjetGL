06654696aab5a9b73b3390d9bf56783994d05330
Mariama MANE <mariama.mane5@etu.univ-lorraine.fr>
2021-05-04 19:48:13 +0200

    création getters et setters des class plat, commande et lignecommande

--- src/main/java/gestionRestaurant/Plat.java
+++ src/main/java/gestionRestaurant/Plat.java
@@ -1,1 +1,41 @@
+package gestionRestaurant;
 
+public class Plat {
+	String NomPlat;
+	int prix;
+	String Categorie;
+	int note;
+	public Plat(String nomPlat, int prix, String categorie, int note) {
+		super();
+		NomPlat = nomPlat;
+		this.prix = prix;
+		Categorie = categorie;
+		this.note = note;
+	}
+	public String getNomPlat() {
+		return NomPlat;
+	}
+	public void setNomPlat(String nomPlat) {
+		NomPlat = nomPlat;
+	}
+	public int getPrix() {
+		return prix;
+	}
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+	public String getCategorie() {
+		return Categorie;
+	}
+	public void setCategorie(String categorie) {
+		Categorie = categorie;
+	}
+	public int getNote() {
+		return note;
+	}
+	public void setNote(int note) {
+		this.note = note;
+	}
+	
+	
+}
