ab2aed8583a56d0270605bea471e805adfe8a16c
bouboukane <bouboukane10@gmail.com>
2021-05-05 17:18:08 +0200

    classe client, facture, stockMatièreP

---
+++ src/main/java/gestionRestaurant/Client.java
@@ -0,0 +1,12 @@
+package main.java.gestionRestaurant;
+
+
+public class Client {
+	int idClient;
+	
+	
+	
+	
+	
+}
+	
---
+++ src/main/java/gestionRestaurant/StockMatiereP.java
@@ -0,0 +1,12 @@
+package main.java.gestionRestaurant;
+
+
+public class StockMatiereP {
+	int idMatPremiereP;
+	String nom;
+	double quantite;
+	String TypeQuantite;
+	
+	
+}
+	
