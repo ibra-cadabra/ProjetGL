09e074b429341e385a1b75f51192001167dac004
Aboubacar DOUMBIA <82065656+Abouba71@users.noreply.github.com>
2021-05-28 19:27:08 +0200

    Modification de la classe facture

---
+++ src/main/java/gestionRestaurant/Facture.java
@@ -0,0 +1,64 @@
+package gestionRestaurant;
+
+import java.util.Date;
+
+public class Facture {
+	
+	//Les attributs de la classe
+	
+	private int idFacture;
+	private double montant;
+	private Date date;
+	private Commande cmde;
+	
+	//Constructeur de la classe
+	
+	public Facture(int idFacture, double montant, Date date, Commande cmde) {
+		super();
+		this.idFacture = idFacture;
+		this.montant = montant;
+		this.date = date;
+		this.cmde = cmde;
+	}
+	
+	//Getters and setters
+
+	public int getIdFacture() {
+		return idFacture;
+	}
+
+	public void setIdFacture(int idFacture) {
+		this.idFacture = idFacture;
+	}
+
+	public double getMontant() {
+		return montant;
+	}
+
+	public void setMontant(double montant) {
+		this.montant = montant;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Commande getCmde() {
+		return cmde;
+	}
+
+	public void setCmde(Commande cmde) {
+		this.cmde = cmde;
+	}
+
+	@Override
+	public String toString() {
+		return "Facture [idFacture=" + idFacture + ", montant=" + montant + ", date=" + date + ", cmde=" + cmde + "]";
+	}
+	
+	
+}
