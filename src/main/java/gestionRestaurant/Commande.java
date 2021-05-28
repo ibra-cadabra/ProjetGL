a94bb981fe1ec2fc26a3b6c297e456cb7eef266d
Aboubacar DOUMBIA <82065656+Abouba71@users.noreply.github.com>
2021-05-28 19:32:16 +0200

    Modification de la classe AssistantService et commande

--- src/main/java/gestionRestaurant/Commande.java
+++ src/main/java/gestionRestaurant/Commande.java
@@ -1,21 +1,36 @@
 package gestionRestaurant;
 
 public class Commande {
+	
+	private int idCommande;
 	private String serveur;
 	private double prix;
 	private String article;
 	private String date;
+	
+	//Construit la classe commande
 
-	public Commande(String serveur, double prix, String article, String date) {
+	public Commande(int idCommande, String serveur, double prix, String article, String date) {
 		super();
 		this.serveur = serveur;
 		this.prix = prix;
 		this.article = article;
 		this.date = date;
+		this.idCommande = idCommande;
 	}
 
+	//Getters and setters de la classe commande
+	
 	public String getServeur() {
 		return serveur;
+	}
+
+	public int getIdCommande() {
+		return idCommande;
+	}
+
+	public void setIdCommande(int idCommande) {
+		this.idCommande = idCommande;
 	}
 
 	public void setServeur(String serveur) {
@@ -45,6 +60,13 @@ public class Commande {
 	public void setArticle(String article) {
 		this.article = article;
 	}
+
+	@Override
+	public String toString() {
+		return "Commande [idCommande=" + idCommande + ", serveur=" + serveur + ", prix=" + prix + ", article=" + article
+				+ ", date=" + date + "]";
+	}
+
 	
 	
 	
